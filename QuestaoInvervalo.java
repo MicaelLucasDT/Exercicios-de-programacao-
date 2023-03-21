72882d9eee7766fac39d4e0832d01287094a1085
Micael Lucas <109772685+MicaelLucasDT@users.noreply.github.com>
2023-03-21 11:33:16 -0300

    Add files via upload

---
+++ QuestaoInvervalo.java
@@ -0,0 +1,22 @@
+
+public class QuestaoInvervalo {
+
+	public static void main(String[] args) {
+
+		int a = 450;
+		if (a > 100) {
+
+			if (a < 200) {
+
+				System.out.println(" 'a' está entre 100 e 200! ");
+
+			} else {
+
+				System.out.println("Não está entre 100 e 200! ");
+
+			}
+		} else {
+		}
+
+	}
+}
