d489062ffe0b7abb41424efd7df11233c2290130
Micael Lucas <109772685+MicaelLucasDT@users.noreply.github.com>
2023-04-10 10:34:44 -0300

    Add files via upload

---
+++ ComandodeEscolha.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ComandodeEscolha {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe o sexo: ");
+		String sexo = sc.next().toUpperCase();
+		
+		switch (sexo) {
+		case "M":
+			System.out.println("Masculino");
+			break;
+		case "F":
+			System.out.println("Feminino");
+			break;
+			
+		default:
+			System.out.println("Sexo inválido!");
+			break;
+		}
+	sc.close();
+	}
+
+}
