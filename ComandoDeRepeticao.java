3d65124a750018ff8d17d4fe15d427af6805dcbe
Micael Lucas <109772685+MicaelLucasDT@users.noreply.github.com>
2023-04-10 10:58:46 -0300

    Add files via upload

---
+++ ComandoDeRepeticao.java
@@ -0,0 +1,19 @@
+public class ComandoDeRepeticao {
+
+	public static void main(String[] args) {
+		
+		int x = 1;
+		
+		while (x <= 5) {
+			
+			System.out.println(x + ": Micael");
+			x = x + 1;
+			//x+=1;
+			//x++;
+			//Formas abreviadas.
+			
+		}
+	
+	}
+
+}
