5f97c8cf8c2b286e7bef067b5f5c4efd6f902c53
Micael Lucas <109772685+MicaelLucasDT@users.noreply.github.com>
2023-04-11 19:07:02 -0300

    Add files via upload

---
+++ TabuadaDo2ao10.java
@@ -0,0 +1,97 @@
+
+public class TabuadaDo7 {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		
+		
+		System.out.println("===============TABUADA===============");
+		while (i<=10) {
+			
+			System.out.println("2 x "+ i + " = " + (2*i));
+			i++;
+		}
+
+		System.out.println("=====================================");
+		
+		i = 1;
+		while (i<=10) {
+			
+			System.out.println("3 x "+ i + " = " + (3*i));
+			i++;
+		
+			}
+
+		System.out.println("=====================================");
+		
+		i = 1;
+		while (i<=10) {
+			
+			System.out.println("4 x "+ i + " = " + (4*i));
+			i++;
+		
+			}
+		
+		System.out.println("=====================================");
+		
+		i = 1;
+		while (i<=10) {
+			
+			System.out.println("5 x "+ i + " = " + (5*i));
+			i++;
+		
+			}
+		
+		System.out.println("=====================================");
+		
+		i = 1;
+		while (i<=10) {
+			
+			System.out.println("6 x "+ i + " = " + (6*i));
+			i++;
+		
+			}
+		
+		System.out.println("=====================================");
+		
+		i = 1;
+		while (i<=10) {
+			
+			System.out.println("7 x "+ i + " = " + (7*i));
+			i++;
+		
+			}
+		
+		System.out.println("=====================================");
+		
+		i = 1;
+		while (i<=10) {
+			
+			System.out.println("8 x "+ i + " = " + (8*i));
+			i++;
+		
+			}
+		
+		System.out.println("=====================================");
+		
+		i = 1;
+		while (i<=10) {
+			
+			System.out.println("9 x "+ i + " = " + (9*i));
+			i++;
+		
+			}
+		
+		System.out.println("=====================================");
+		
+		i = 1;
+		while (i<=10) {
+			
+			System.out.println("10 x "+ i + " = " + (10*i));
+			i++;
+		
+			}
+		
+	}
+}
