e578cabc6e40492ebe0eb88840b00cdad3c6c1eb
Micael Lucas <109772685+MicaelLucasDT@users.noreply.github.com>
2023-04-11 11:25:54 -0300

    Imprimir números entre 11, 21, 31 ... 101

---
+++ ImprimirNumerosEntre112131.java
@@ -0,0 +1,17 @@
+
+public class ImprimirNumerosEntre112131 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 11; i <= 101; i = i + 10) {
+			
+			System.out.println( i + " ");
+			
+		}
+		
+		
+		
+
+	}
+
+}
